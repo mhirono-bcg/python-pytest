6cf43836824499167e58c1206f329a08fdb274f3
Hirono.Masatake@bcg.com <Hirono.Masatake@bcg.com>
2022-05-13 05:40:36 +0900

    exercise

---
+++ ch1/test_exercise.py
@@ -0,0 +1,13 @@
+def test_exercise_1():
+    l = 2
+    assert l in [2, 3, 4]
+
+
+def test_exercise_2():
+    a = 1
+    b = 2
+    assert a < b
+
+
+def test_exercise_3():
+    assert "fizz" not in "fizzbuzz"
